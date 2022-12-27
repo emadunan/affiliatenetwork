c53dc49c1b155537dbc34323e79f6cd4e054e1b0
emadunan <emadunan@gmail.com>
2022-12-27 20:38:23 +0200

    fix: check to none

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,12 +13,12 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      checks: "state"
+      checks: "none"
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
-      checks: "state"
+      checks: "none"
     })
   ],
   debug: true,
