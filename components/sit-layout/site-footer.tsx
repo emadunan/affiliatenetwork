c9a868f67168b94f694046041068511d5297a5df
emadunan <emadunan@gmail.com>
2023-01-20 00:47:18 +0200

    refactor: Reduce footer size

--- components/sit-layout/site-footer.tsx
+++ components/sit-layout/site-footer.tsx
@@ -10,12 +10,14 @@ const SiteFooter: FC<SiteFooterProps> = (props) => {
   return (
     <footer className={`${props.className}`}>
       <Container>
-        <div className="h-20 flex justify-center items-center">
+        <div className="h-10 flex justify-between items-center">
+          <Link href={"/privacy-policy"}>
+            <h4 className="my-2 hover:text-white">Privacy Policy</h4>
+          </Link>
           <h5>Copy rights reserved © 2023</h5>
+          <h4>.</h4>
         </div>
-        <Link href={"/privacy-policy"}>
-          <h4 className="my-2 hover:text-white">Privacy Policy</h4>
-        </Link>
+
       </Container>
     </footer>
   );
